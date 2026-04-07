fd2a2d896e9da8f88fc14bc2f30d323cdf950936
helppy <5542jgxjjt@privaterelay.appleid.com>
2026-04-07 10:16:05 +0900

    Revert "feat:student dao 구현"

--- src/com/tenco/library/dto/Student.java
+++ src/com/tenco/library/dto/Student.java
@@ -13,9 +13,4 @@ public class Student {
     private String name;
     private String studentId;
 
-    public Student(String name, String studentId) {
-        this.name = name;
-        this.studentId = studentId;
-    }
-
 }
